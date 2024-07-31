8d833ee5ac3fe9a02676222526197503e5fd17f3
utyy03 <putrirezekika40@gmail.com>
2024-07-31 15:02:47 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("testimonial-slide");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+// Automatic slideshow
+setInterval(function() {
+    plusSlides(1);
+}, 5000);
+
+document.querySelector(".testimonial-prev").addEventListener("click", function() {
+    plusSlides(-1);
+});
+
+document.querySelector(".testimonial-next").addEventListener("click", function() {
+    plusSlides(1);
+});
